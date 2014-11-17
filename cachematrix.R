41855c2c787e6bbefc4f67cf5828bc0100ff4b8b
PavoDive <gp@optimizamos.com>
2014-11-17 08:20:10 -0500

    PavoDive solution for assignment 2 of R Programming Coursera course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Code for assignment 2 of R programming.
+# This function calculates the inverse of [potentially] big matrixes, and chaches it, so it can compare whether matrix has changed
+# and re-calculate inverse, or return cached value if not
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setInverse<-function(inverse) I<<-inverse
+  getInverse<-function()I
+  list(set=set,get=get,getInverse=getInverse,setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
+
+## Determines whether it already has the value in cache, or solves (produces the inverse) of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  I<-x$getInverse()
+  if(!is.null(I)){
+    message("getting cached data")
+    return(I)
+  }
+  data<-x$get()
+  I<-solve(data)
+  x$setInverse(I)
+  I
+} 
